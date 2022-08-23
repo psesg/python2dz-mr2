a21419dcb6bd34ccbadfc57adf1335e17acfc9e4
serg <pseesp@mail.ru>
2022-08-23 17:52:30 +0300

    +=

--- mapper_py2.py
+++ mapper_py2.py
@@ -12,12 +12,17 @@ with open("stop_words_en.txt") as fd:
     for line in fd:
         stop_words.add(line.strip())
 
+mx = 0
+mxstr = ''
+mxnumline = 0
+numline = 0
 
 for line in sys.stdin:
     try:
         article_id, text = unicode(line.strip()).split('\t', 1)
     except ValueError as e:
         continue
+    numline += 1
     text = re.sub(r'[^A-Za-z\\s]', ' ', text)
     words = re.split("\W*\s+\W*", text, flags=re.UNICODE)
     for word in words:
@@ -26,7 +31,12 @@ for line in sys.stdin:
         if word in stop_words or len(word) < 3:
             # print >> sys.stderr, "reporter:counter:Custom stats,Stop words,{}".format(1)
             continue
+        if len(word) > mx:
+            mx = len(word)
+            mxstr = word
+            mxnumline = numline
         print "%s\t%d" % (word, 1)
+print "mx = %d %s" % (mx, mxstr)
 
 
 
