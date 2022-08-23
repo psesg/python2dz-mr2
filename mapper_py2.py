54c8d527fe52c7394840d2534e704015909b240d
serg <pseesp@mail.ru>
2022-08-23 17:06:37 +0300

    mapper+

--- mapper_py2.py
+++ mapper_py2.py
@@ -18,11 +18,12 @@ for line in sys.stdin:
         article_id, text = unicode(line.strip()).split('\t', 1)
     except ValueError as e:
         continue
+    text = re.sub(r'[^A-Za-z\\s]', ' ', text)
     words = re.split("\W*\s+\W*", text, flags=re.UNICODE)
     for word in words:
         word = word.lower()
         # print >> sys.stderr, "reporter:counter:Custom stats,All words,{}".format(1)
-        if word in stop_words or len(word) > 3:
+        if word in stop_words or len(word) < 3:
             # print >> sys.stderr, "reporter:counter:Custom stats,Stop words,{}".format(1)
             continue
         print "%s\t%d" % (word, 1)
