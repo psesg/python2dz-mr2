3f03c35b17f7346298e5c94e118fdfec14093c07
Serg <pseesp@mail.ru>
2022-08-23 15:29:52 +0300

    mr2 - begin

--- mapper_py2.py
+++ mapper_py2.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python
 
+
 import sys
-import string
-import random
-
-
-def randstr(chars=string.hexdigits, n=5):
-    return ''.join(random.choice(chars) for _ in xrange(n))
-
+import re
 
 reload(sys)
-sys.setdefaultencoding('utf-8')  # required to convert to unicode
+sys.setdefaultencoding('utf-8') # required to convert to unicode
+
+stop_words = set()
+with open("stop_words_en.txt") as fd:
+    for line in fd:
+        stop_words.add(line.strip())
+
 
 for line in sys.stdin:
     try:
-        article_id = unicode(line.strip("\n"))
+        article_id, text = unicode(line.strip()).split('\t', 1)
     except ValueError as e:
         continue
-    print "%s%s" % (randstr(), article_id)
+    words = re.split("\W*\s+\W*", text, flags=re.UNICODE)
+    for word in words:
+        word = word.lower()
+        # print >> sys.stderr, "reporter:counter:Custom stats,All words,{}".format(1)
+        if word in stop_words or len(word) > 3:
+            # print >> sys.stderr, "reporter:counter:Custom stats,Stop words,{}".format(1)
+            continue
+        print "%s\t%d" % (word, 1)
+
+
+
