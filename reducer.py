a8a4062414332f51edd768e4ff24e750a1ee6d8e
serg <pseesp@mail.ru>
2022-08-24 16:56:59 +0300

    all works and on cluster! ren to mapper.py and reducer.py

--- reducer.py
+++ reducer.py
@@ -4,7 +4,7 @@ import sys
 import operator
 from collections import OrderedDict
 
-# python mapper_py2.py < articles-part.txt | sort | python reducer_py2.py | sort -k2,2nr -k1 | head -10
+# python mapper.py < articles-part.txt | sort | python reducer.py | sort -k2,2nr -k1 | head -10
 
 reload(sys)
 sys.setdefaultencoding('utf-8')  # required to convert to unicode
